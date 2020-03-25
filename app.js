0e59d002a6d90d8dea93b9e19e4015d6b54bb7d5
Vitor Carrilho <vitor.carrilho@hotmail.com>
2020-03-25 15:51:42 -0300

    instancia do socket io

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // Importar as configurações do servidor
 var app = require('./config/server');
-
+var socket = require('socket.io');
 // Parametrizar a porta de escuta
 
-app.listen(80, function(){
+var server = app.listen(80, function(){
     console.log('Servidor online');
 });
+
+var io = socket.listen(server);
+
+io.on('connect',function(socket){
+    console.log("Usuário conectado")
+
+    socket.on('disconnect',function(){
+        console.log("Desconectado")
+    })
+})
